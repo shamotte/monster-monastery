c4db96a58e8682e4b43d8c30fc76b6f590df9ace
shamotte <123325697+shamotte@users.noreply.github.com>
2024-04-18 00:52:20 +0200

    resources

---
+++ resources/resource_resource.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name ResourceResource
+
+@export_group("Resource")
+@export var name:String
+@export var sprite:CompressedTexture2D
+
+@export_group("Resource Point")
+@export var action_type: Priorities.ACTIONTYPES
+@export var time:float
+@export var resource_point_txture: CompressedTexture2D
