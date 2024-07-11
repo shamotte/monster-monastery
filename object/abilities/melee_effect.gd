4fca37a826ac30b1fe90961f09a1ebbf583dd3b3
shamotte <123325697+shamotte@users.noreply.github.com>
2024-07-11 19:45:00 +0200

    making melee disappear

--- object/abilities/melee_effect.gd
+++ object/abilities/melee_effect.gd
@@ -1,11 +1,12 @@
 extends Sprite2D
 
-@export var attack_duration : float 	#how long streak will appear
+@export var attack_duration : float  = 0.4	#how long streak will appear
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#texture = null
-	$AnimSprite.play("Attack")
+	if has_node("AnimSprite"):
+		$AnimSprite.play("Attack")
 	await get_tree().create_timer(attack_duration).timeout
 	queue_free() # Replace with function body.
 
