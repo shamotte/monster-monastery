c2e7bcc16bbf567e03e7f4c0036235921ed61884
Zed-z <71879051+Zed-z@users.noreply.github.com>
2024-07-11 11:59:36 +0200

    Shadow fixes

--- scripts/daylight_manager.gd
+++ scripts/daylight_manager.gd
@@ -8,19 +8,17 @@ func _ready():
 func _physics_process(delta):
 	if $AnimationPlayer.current_animation != "":
 		var daylight_progress = $AnimationPlayer.current_animation_position / $AnimationPlayer.current_animation_length
-		print(daylight_progress)
-		
+
 		for shadow in get_tree().get_nodes_in_group("shadow"):
 			shadow.set_skew(deg_to_rad(lerp(-90.0, 90.0, daylight_progress)))
-			shadow.flip_v = 1
-			shadow.set_position(Vector2(-16, 0).rotated(PI * daylight_progress) + Vector2(0, 16))
-			shadow.set_offset(Vector2(0, shadow.texture.get_height()))
+			shadow.set_position(Vector2(shadow.texture.get_height() / 2, 0).rotated(PI * daylight_progress) + Vector2(0, shadow.texture.get_height() / 2))
+			shadow.set_offset(Vector2(0, -shadow.texture.get_height()))
 			
 	
 func start_cycle():
 	visible = true
 	$AnimationPlayer.play("cycle")
-	$AnimationPlayer.speed_scale = 10.0/12.0
+	$AnimationPlayer.speed_scale = 10.0/120.0
 
 func _on_animation_player_animation_finished(anim_name):
 	$AnimationPlayer.play("cycle")
