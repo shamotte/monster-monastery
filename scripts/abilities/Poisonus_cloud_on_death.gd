10b4be7dc3e323c1f9bc314b5d25430c085b2721
shamotte <123325697+shamotte@users.noreply.github.com>
2024-08-14 23:14:26 +0200

    2 new unit abilities

---
+++ scripts/abilities/Poisonus_cloud_on_death.gd
@@ -0,0 +1,13 @@
+extends Ability
+class_name CloudOnDeath
+@export  var scene : PackedScene
+
+
+
+func on_death():
+	var instance : Area2D = scene.instantiate()
+	instance.collision_mask = to_target
+	instance.global_position = ovner.global_position
+	ProjectileOverseeer.add_child(instance)
+
+
--- scripts/abilities/shock_wave.gd
+++ scripts/abilities/shock_wave.gd
@@ -1,11 +1,34 @@
-extends Node
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+class_name ShockWave
+extends Ability
+@export var  cooldown : float
+@export var visual_effect :PackedScene
+@export var damage: float = 10
+@export var range:float = 30 :
+	get:
+		return range /2
+var time_left = 0
+func fight_process(delta):
+	time_left -= delta
+	if time_left <= 0:
+		
+		if ovner.global_position.distance_to(ovner.target_global_position) < range:
+			time_left = cooldown
+			var effect = visual_effect.instantiate()
+			var direction = (ovner.target_global_position - ovner.global_position).normalized()
+			effect.global_position = ovner.global_position
+			var space:PhysicsDirectSpaceState2D = ProjectileOverseeer.get_world_2d().direct_space_state
+			var parameters : PhysicsShapeQueryParameters2D =  PhysicsShapeQueryParameters2D.new()
+			var shape := CircleShape2D.new()
+			shape.radius = range
+			
+			parameters.shape = shape
+			parameters.collide_with_areas = false
+			parameters.collision_mask = to_target
+			parameters.transform = Transform2D(Vector2.UP.angle_to(direction),ovner.global_position)
+		
+			var result = space.intersect_shape(parameters)
+			
+			for col in result:
+				col.collider.take_damage(damage)
+			
+			ProjectileOverseeer.add_child(effect);
