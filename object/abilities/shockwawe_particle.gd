bafe47ddd86804520a1050a6c5387147f07cd20c
shamotte <123325697+shamotte@users.noreply.github.com>
2024-09-10 13:48:51 +0200

    particle lag fix

--- object/abilities/shockwawe_particle.gd
+++ object/abilities/shockwawe_particle.gd
@@ -1,9 +1,9 @@
-extends GPUParticles2D
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	emitting = true
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
