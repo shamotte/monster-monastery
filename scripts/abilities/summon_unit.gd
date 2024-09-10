8be2c6ee08df87fa70d6310feb8dadada15a3e8d
shamotte <123325697+shamotte@users.noreply.github.com>
2024-09-10 16:17:31 +0200

    updated summoning

--- scripts/abilities/summon_unit.gd
+++ scripts/abilities/summon_unit.gd
@@ -5,7 +5,7 @@ extends Ability
 var time_left :float  = 0
 var range = 100
 @export var summon_range : int = 30
-
+@export var max_summons : int = 1
 @export var summon_duration : float  = 10
 
 @export var should_despawn : bool = true
@@ -16,17 +16,23 @@ func init(caster):
 	time_left = cooldown
 	instance = preload("res://object/unit.tscn")
 
+var spawned_units = 0
 func process(delta):
 	time_left -= delta
 	if time_left <=0:
-		time_left = cooldown
-		var i :Unit = instance.instantiate()
-		i.type = type
-		i.global_position = ovner.global_position + Vector2(randf_range(-1,1),randf_range(-1,1))* summon_range
-		ProjectileOverseeer.get_node("../Game").unit_holder.add_child(i)
-		if should_despawn:
-			await ovner.get_tree().create_timer(summon_duration).timeout
-			if is_instance_valid(i):
-				i.queue_free()
-		
+		if spawned_units < max_summons:
+			spawned_units += 1
+			time_left = cooldown
+			var i :Unit = instance.instantiate()
+			i.type = type
+			i.unit_died.connect(unit_died(),CONNECT_ONE_SHOT)
+			i.global_position = ovner.global_position + Vector2(randf_range(-1,1),randf_range(-1,1))* summon_range
+			ProjectileOverseeer.get_node("../Game").unit_holder.add_child(i)
+			if should_despawn:
+				await ovner.get_tree().create_timer(summon_duration).timeout
+				if is_instance_valid(i):
+					i.queue_free()
+			
 	
+func unit_died():
+	spawned_units -= 1 
